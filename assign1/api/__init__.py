c7f61603a410e4d942562496dabe9720f7898f5b
Anurat Bhattacharya <anuratb@yahoo.com>
2023-01-22 22:14:14 +0530

    basic prototype

--- assign1/api/__init__.py
+++ assign1/api/__init__.py
@@ -1,10 +1,15 @@
-import psycopg2
-from sqlalchemy import create_engine 
-from sqlalchemy.orm import scoped_session, sessionmaker
-DB_URI = 'postgresql+psycopg2://anurat:abcd@127.0.0.1:5432/anurat'
-engine = create_engine(DB_URI) 
-db = scoped_session(sessionmaker(bind=engine))
 
-connection = engine.connect() 
-#result = connection.execute('select * from table') 
-#print(connection)
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+DB_URI = 'postgresql+psycopg2://anurat:abcd@127.0.0.1:5432/anurat'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+db = SQLAlchemy(app)
+
+
+
---
+++ assign1/api/routes.py
@@ -0,0 +1,42 @@
+
+from flask import   request, jsonify
+from api import app,db
+
+import flask
+
+'''
+Method: POST
+Endpoint: /topics
+Params:
+- "name": <string>
+Response:
+onSuccess:
+- "status": “success”
+- "message": <string>
+onFailure:
+- "status": “failure”
+- "message": <string> // informative error message
+
+
+Method: GET
+Endpoint: /topics
+Params:
+None
+Response:
+"status": <string>
+onSuccess:
+- "topics": List[<string>] // List of topic names
+onFailure:
+- "message": <string> // Error message
+'''
+@ app.route("/topics", methods=[ 'GET','POST'])
+def topics():
+    if(flask.request.method=='GET'):
+        pass
+    else:
+        pass
+
+
+
+
+
