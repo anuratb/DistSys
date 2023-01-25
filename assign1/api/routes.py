3df0960ad2dcd894bf43ef503c363553651d262d
girinath <girinath2452001@gmail.com>
2023-01-25 17:22:03 +0530

    Corrections to route.py

--- assign1/api/routes.py
+++ assign1/api/routes.py
@@ -1,7 +1,7 @@
 
 from flask import request, jsonify
 from api import app, db
-from data_struct import *
+from api.data_struct import *
 
 '''
     a. CreateTopic
@@ -175,7 +175,7 @@ def size():
     except Exception as e: 
         return {
             "status" : "Failure" ,
-             "message" : e
+             "message" : str(e)
             }
 
 
