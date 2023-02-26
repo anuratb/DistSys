7f4a4544be966241581bbe0ec4c9ef1b5f1ca691
Anurat Bhattacharya <anuratb@yahoo.com>
2023-02-27 02:15:00 +0530

    created dbms schema for metadata

--- assign2/manage_brokers/api/models.py
+++ assign2/manage_brokers/api/models.py
@@ -11,3 +11,78 @@ Topic
 Partition 
 
 '''
+from api import db
+
+
+####################### FOR TOPIC METADATA #########################
+class TopicMetadataDB(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    PartitionBroker = db.relationship('TopicBroker',backref='topic',lazy =True)
+    BrokerUrls = db.relationship('BrokerURL',backref='topic',lazy=True)
+    pass
+
+class TopicBroker(db.model):
+    topic = db.Column(db.String,primary_key=True,nullable=False)
+    brokerURL = db.Column(db.String,primary_key = False,nullable=False)
+    topic_id = db.Column(db.Integer,db.ForeignKey('TopicMetadataDB.id'))
+
+class BrokerURL(db.model):
+    brokerURL = db.Column(db.string,primary_key=True,nullable = False)
+    topic_id = db.Column(db.Integer,db.ForeignKey('TopicMetadataDB.id'))
+
+
+######################### FOR PRODUCER METADATA #################################
+class ProducerMetaDataDB(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    subscription = db.relationship('ProdSubscribe',backref='prodMetaData',lazy=True)
+    
+
+class ProdSubscribe(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    rrIndex = db.Column(db.Integer,nullable=False)
+    brokerList = db.relationship('ProdTopicBroker',backref='ProdSubscribe',lazy = True)
+    prodMetadataId = db.Column(db.Integer,db.ForeignKey('ProducerMetadata.id'))
+
+class ProdTopicBroker(db.model):
+    brokerUrl = db.Column(db.Integer,primary_key=True,nullable=False)
+    prodId = db.Column(db.Integer,nullable=False)
+    ProdSubscribeId = db.Column(db.Integer,db.ForeignKey('ProdSubscribe.id'))
+
+######################### FOR CONSUMER METADATA #################################
+class ConsumerMetaDataDB(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    subscription = db.relationship('ConSubscribe',backref='conMetaData',lazy=True)
+    
+
+class ConSubscribe(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    rrIndex = db.Column(db.Integer,nullable=False)
+    brokerList = db.relationship('ConTopicBroker',backref='ConSubscribe',lazy = True)
+    conMetadataId = db.Column(db.Integer,db.ForeignKey('ConsumerMetadata.id'))
+
+class ConTopicBroker(db.model):
+    brokerUrl = db.Column(db.Integer,primary_key=True,nullable=False)
+    conId = db.Column(db.Integer,nullable=False)
+    ConSubscribeId = db.Column(db.Integer,db.ForeignKey('ConSubscribe.id'))
+
+
+
+############################## FOR MANAGER ###############################
+#class ManagerDB(db.model):
+#    id = db.Column(db.Integer,primary_key=True,nullable=False)
+
+
+############################### BROKER META DATA ##################
+
+class BrokerMetaDataDB(db.model):
+    db_uri = db.Column(db.String,primary_key=True,nullable=False)
+    url = db.Column(db.String,nullable=False)
+    docker_name = db.Column(db.String,nullable=False)
+    last_beat = db.Column(db.Float,nullable=False)
+    docker_id = db.Column(db.Integer,db.ForeignKey('DockerDB.id'))
+
+############################### DOCKER METADATA ##############################################
+class DockerDB(db.model):
+    id = db.Column(db.Integer,primary_key=True,nullable=False)
+    brokers = db.relationship('BrokerMetaDataDB',backref='docker',lazy=True)
+    
