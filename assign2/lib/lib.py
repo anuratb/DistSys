9cfcbd41655e2fb92831ed8a22104969de5845b7
Anindya-2000 <sikdarani01@gmail.com>
2023-03-05 14:27:22 +0530

    TESTING

---
+++ assign2/lib/lib.py
@@ -0,0 +1,231 @@
+import requests
+
+class MyQueue:
+    
+    def __init__(self,url:str):
+        self.url = url
+    '''
+    Builder function to create topic
+    @param topicName 
+    @return Topic object
+    '''
+
+    def createTopic(self,topicName:str):
+        try:
+            #print(str("^^^^^^")+topicName)
+            res = requests.post(self.url+"/topics",json={
+                "name":topicName
+            })
+            if(res.json().get("status")=="Success"):
+                return self.Topic(self,topicName)
+            else:
+                #print(str(res.json()))
+                raise Exception(res.json().get("message"))
+
+        except Exception as err:
+            return str(err)
+
+    '''
+    Method To get all Topics
+    '''
+    def get_all_topics(self):
+        try:
+            res = requests.get(self.url+"/topics")
+            if(res.json().get("status")=="Success"):
+                return res.json().get("topics")
+            else:
+                raise Exception(res.json().get("message"))
+        except Exception as err:
+            return str(err)
+
+    '''
+    Builder function to create producer from topiclist
+    @param topicNames
+    @return Producer Object
+    '''
+    def createProducer(self,topicNames:list):
+        try:
+            ids = {}
+            for topicName in topicNames:
+                # Check if producer is already registered in topicName
+                if topicName in ids: continue
+                res = requests.post(
+                    self.url+"/producer/register",
+                    json={
+                        "topic":topicName
+                    })
+                if(res.json().get("status")!="Success"):
+                    raise Exception(res.json().get("message"))
+                else:
+                    pid = res.json().get("producer_id")
+                    ids[topicName] = pid
+            return self.Producer(self,ids)                
+
+        except Exception as err:
+            return str(err)
+    '''
+    Builder function to create consumer
+    @param topicNames
+    @return Consumer Object
+    '''
+    def createConsumer(self,topicNames:list):
+        try:
+            ids = {}
+            for topicName in topicNames:
+                # Check if consumer is already registered in topicName
+                if topicName in ids: continue
+                res = requests.post(
+                    self.url+"/consumer/register",
+                    json={
+                        "topic":topicName
+                    })
+                if(res.json().get("status")!="Success"):
+                    raise Exception(res.json().get("message"))
+                else:
+                    cid = res.json().get("consumer_id")
+                    ids[topicName] = cid
+            return self.Consumer(self,ids)                
+
+        except Exception as err:
+            raise str(err)
+    '''
+    Topic class
+    '''
+    class Topic:
+        def __init__(self,outer,topicName:str):
+            self.topicName = topicName
+            self.outer = outer
+        def __str__(self) -> str:
+            print(self.topicName)
+    '''
+    Producer Class
+    '''
+    class Producer:
+
+        def __init__(self,outer, pids:dict):
+            #self.topicName = topicName
+            self.pids = pids
+            self.outer = outer
+        '''
+        To add a new topic(already existing in db) to topicList of producer
+        @param topicName
+        
+        '''
+        def registerTopic(self, topicName: str):
+            try:
+                # Check if producer is already registered in topicName
+                if topicName in self.pids: return
+                res = requests.post(
+                    self.url+"/producer/register",
+                    json={
+                        "topic": topicName
+                    }
+                )
+                if(res.json().get("status") != "Success"):
+                    raise Exception(res.json().get("message"))
+                else:
+                    pid = res.json().get("producer_id")
+                    self.pids[topicName] = pid
+            except Exception as err:
+                return str(err)
+        '''
+        To enqueue a message
+        @param msg : Message
+        @param topicName: topic Name
+        returns 0 if success
+        '''
+        def enqueue(self,msg:str,topicName:str):
+            if(topicName not in self.pids.keys()):
+                raise Exception("Error: Topic {} not registered".format(topicName))
+            try:
+                id = self.pids[topicName]
+                res = requests.post(
+                    self.outer.url+"/producer/produce",
+                    json={
+                        "topic":topicName,
+                        "producer_id":id,
+                        "message":msg
+                    }
+                )
+                if(res.json().get("status")=="Success"):
+                    return 0
+                else:
+                    raise Exception(res.json.get("message"))
+            except Exception as err:
+                return str(err)
+    '''
+    Consumer class
+    '''
+    class Consumer:
+
+        def __init__(self,outer,cids:dict):
+            #self.topicName = topicName
+            self.cids = cids
+            self.outer = outer
+
+        # Used to register for a topic
+        def registerTopic(self, topicName: str):
+            try:
+                # Check if producer is already registered in topicName
+                if topicName in self.cids: return
+                res = requests.post(
+                    self.url+"/consumer/register",
+                    json={
+                        "topic": topicName
+                    }
+                )
+                if(res.json().get("status") != "Success"):
+                    raise Exception(res.json().get("message"))
+                else:
+                    cid = res.json().get("consumer_id")
+                    self.cids[topicName] = cid
+            except Exception as err:
+                return str(err)
+        '''
+        To dequeue from a topic subscribed by consumer
+        @param topicName
+        @return The message dequeued
+        '''
+        def dequeue(self,topicName:str):
+            if(topicName not in self.cids.keys()):
+                raise Exception("Error: Topic not registered")
+            try:
+                id = self.cids[topicName]
+                res = requests.get(
+                    self.outer.url+"/consumer/consume",
+                    params={
+                        "topic":topicName,
+                        "consumer_id":id
+                    }
+                )
+                if(res.json().get("status")=="Success"):
+                    return res.json().get("message")
+                else:
+                    raise Exception(res.json.get("message"))
+            except Exception as err:
+                return str(err)
+        '''
+        Method to get queue size belonging to some topic name
+        @param topicName
+        @return size of the queue
+        '''
+        def getSize(self,topicName):
+            if(topicName not in self.cids.keys()):
+                raise Exception("Error: Topic not registered")
+            try:
+                res = requests.get(
+                    self.outer.url+"/size",
+                    params={
+                        "topic":topicName,
+                        "consumer_id":self.cids[topicName]
+                    }
+                )
+                if(res.json().get("status")=="Success"):
+                    return int(res.json().get("size"))
+                else:
+                    raise Exception(str(res.json().get("message")))
+                
+            except Exception as err:
+                return str(err)
+
+
--- assign2/test_api.py
+++ assign2/test_api.py
@@ -2,109 +2,12 @@ from lib.lib import *
 import os
 import threading
 import random,time
+
 def run_test(url="http://127.0.0.1:5123"):
     Q = MyQueue(url)
-    path = "./test_asgn1"
-    fp = open(path+"/prod_cons.txt")
-    topics = []
-    prods = {}
-    cons = {}
-    for line in fp.readlines():
-        line = line.split(':')
-        for wrd in line:
-            wrd = wrd.strip()
-        topic = line[0]
-        prods_ = line[1].split()
-        cons_ = line[2].split()
-        print(topic)
-        while(True):
-            try:
-                Q.createTopic(topic)
-                break
-            except:
-                continue
-        topics.append(topic)
-        for prod in prods_:
-            if(prod not in prods.keys()): prods[prod] = []
-            prods[prod].append(topic)
-        for con in cons_:
-            if(con not in cons.keys()): cons[con] = []
-            cons[con].append(topic)
-    pobj = {}
-    conobj = {}
-    print(prods.keys())
-    while(True):
-        try:
-            for prod,topics in prods.items():
-                pobj[prod] = Q.createProducer(topics)
-            for con,topics in cons.items():
-                conobj[con] = Q.createConsumer(topics)
-            break
-        except:
-            continue
-    
-    prod_msg = {}
-    def produce(prod):
-        print("Running Pthread "+str(prod_msg[prod][:5]))
-        i = 0
-        while(i<len(prod_msg[prod])):
-            try:
-                msg,topic = prod_msg[prod][i]
-                cur:MyQueue.Producer = pobj[prod]
-                delay = random.random()
-                time.sleep(delay)
-                cur.enqueue(msg,topic)
-                i+=1
-            except:
-                continue
-        '''
-        for msg,topic in prod_msg[prod]:
-            cur:MyQueue.Producer = pobj[prod]
-            delay = random.random()
-            time.sleep(delay)
-            cur.enqueue(msg,topic)
-        '''
-    def consume(con):
-        cur:MyQueue.Consumer = conobj[con]
-        while(True):
-            try:
-                for topic in cons[con]:
-                    delay = random.random()
-                    time.sleep(delay)
-                    
-                    if(cur.getSize(topic)>0):
-                        msg = cur.dequeue(topic)
-                        print("Dequeued: {} by consumer {}".format(msg,con))
-                
-            except:
-                continue
-             
-    for file in os.listdir(path):
-        nm,ext = file.split('.')
-        tp,id = nm.split('_')
-        if(tp=="producer" and ext== "txt"):
-            prod_msg["P"+str(id)] = []
-            for line in open(path+"/"+file).readlines():
-                #print(line.split())
-                tm,msg,_,topic = line.split()
-                topic = "T"+topic[2:3]
-                prod_msg["P"+str(id)].append((line,topic))
-    print(prod_msg.keys())
-    pt = []
-    ct = []
-    for prod in prods.keys():
-        pt.append(threading.Thread(target=produce,args=(prod,)))
-    for con in cons.keys():
-        ct.append(threading.Thread(target=consume,args=(con,)))
-    for t in pt:
-        t.start()
-    
-    for c in ct:
-        c.start()
-    for t in pt:
-        t.join()
-    for c in ct:
-        c.join()
+    t = Q.createTopic("News0")
+    print(t)
+
 if(__name__=='__main__'):
     run_test()
     
