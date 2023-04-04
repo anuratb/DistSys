c1da4869fbe2d8d079358788d2ffb99a2ac0aa8e
srijanakde2001 <srijanak_de@yahoo.in>
2023-04-04 12:30:30 +0530

    Implemented accounts in ass3 partA

--- assign3/ATM/main.py
+++ assign3/ATM/main.py
@@ -32,3 +32,48 @@ class User(SyncObj):
     
     def getUserList(self):
         print(f"## userList: {self.userList}")
+
+    
+class Account(SyncObj):
+
+    def __init__(self, selfNodeAddr, otherNodeAddrs):
+        super(Account, self).__init__(selfNodeAddr, otherNodeAddrs)
+        # AccountNo -> Balance
+        self.accountList = {}
+        self.ID = 0
+
+    @replicated
+    def createAccount(self):
+        ID_ = str(self.ID)
+        self.ID += 1
+        self.accountList[ID_] = 0
+        return ID_
+
+    @replicated
+    def withDrawal(self, accountNo, amount):
+        if amount > self.accountList[accountNo]:
+            return "## Error: Not enough Balance!"
+        self.accountList[accountNo] -= amount
+        return f"$$ Updated balance: {self.accountList[accountNo]}"
+    
+    @replicated
+    def deposit(self, accountNo, amount):
+        self.accountList[accountNo] += amount
+        return f"$$ Updated balance: {self.accountList[accountNo]}"
+
+    @replicated
+    def fundTransfer(self, accountTo, accountFrom, amount):
+        if accountTo not in self.accountList:
+            return f"## Error: Account Number {accountTo} doesn't exist"
+        if amount > self.accountList[accountFrom]:
+            return "## Error: Not enough Balance!"
+        self.accountList[accountFrom] -= amount
+        self.accountList[accountTo] += amount
+        return f"$$ Transfer successful. Updated Balance: {self.accountList[accountFrom]}"
+
+    def balanceEnquiry(self, accountNo):
+        return self.accountList[accountNo]
+
+    def getAllAccounts(self):
+        print(f"## accountList: {self.accountList}")
+        print(f"## Latest Account ID: {self.ID}")
