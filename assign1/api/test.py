211ca26f46ab9cbd3cd72edcae91e9e1c9fc5b19
Anurat Bhattacharya <anuratb@yahoo.com>
2023-04-06 19:24:35 +0530

    changes

--- assign1/api/test.py
+++ assign1/api/test.py
@@ -1,19 +1,23 @@
-from pysyncobj import SyncObj,replicated
+from pysyncobj import SyncObj, replicated
 
 class Counter(SyncObj):
     def __init__(self):
-        super().__init__('127.0.0.1:9090', ['127.0.0.1:9091'])
-        self.value = 0
-    @replicated 
-    def increment(self):
-        self.value += 1
-       # self.sync()
-        
-    def get_value(self):
-        return self.value
-if __name__=='__main__':
-    counter = Counter()
-    counter.increment()
-    print(counter.get_value())
-    counter.increment()
-    print(counter.get_value())
+        super().__init__('localhost:4321', ['localhost:4322'])
+        self.counter = 0
+
+    @replicated
+    def increment_counter(self):
+        self.counter += 1
+
+    def get_counter(self):
+        return self.counter
+
+
+counter = Counter()
+
+print(counter.get_counter())  # Output: 0
+
+counter.increment_counter()
+counter.increment_counter()
+
+print(counter.get_counter())  # Output: 2
